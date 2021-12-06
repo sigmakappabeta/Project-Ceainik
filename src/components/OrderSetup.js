ad33a0850b7ab06a0dd9f17d6378be5c46bab71c
silviuceban <silviuceban@gmail.com>
2021-12-06 10:13:55 +0200

    added logit to TOTAL price block

--- src/components/OrderSetup.js
+++ src/components/OrderSetup.js
@@ -1,7 +1,9 @@
 import React, { useState, createContext, useEffect } from "react";
 import GreenCheckmark from "./images/green_check_mark.png";
 
-import PinkHeart from "./images/pink_heart.png";
+import pink_heart from "./images/pink_heart.png";
+import likeHover from "./images/like_hover.png";
+import liked from "./images/liked.png";
 import SizeOption from "./SizeOption";
 import SpecialOption from "./SpecialOption";
 
@@ -9,7 +11,7 @@ export const SelectedSizeOption = createContext(null);
 export const SelectedSpecialOption = createContext(null);
 export default function OrderSetup() {
   //==========PRODUCT OBJECT=============
-  let i = 0;
+
   const product = {
     name: "BUCHET_NO_#1",
     description:
@@ -20,17 +22,76 @@ export default function OrderSetup() {
       { size: "mic", price: "650", id: "3" },
     ],
     specs: [
-      { img: "teddy_1.png", price: "123", id: "1" },
-      { img: "teddy_1.png", price: "343", id: "2" },
-      { img: "teddy_1.png", price: "563", id: "3" },
-      { img: "teddy_1.png", price: "783", id: "4" },
+      { name: "Ursuleț de pluș", img: "teddy_1.png", price: "123", id: "1" },
+      {
+        name: "Ursuleț de catifea",
+        img: "teddy_1.png",
+        price: "343",
+        id: "2",
+      },
+      { name: "Ursuleț de roșu", img: "teddy_1.png", price: "563", id: "3" },
+      { name: "Ursuleț de alb", img: "teddy_1.png", price: "783", id: "4" },
     ],
   };
 
   //=======================
   const [selected_size, set_selected_size] = useState("1");
   const [selected_special_op, set_selected_special_op] = useState("1");
-  const product_sizes = product.sizes;
+
+  //============================
+  let selected_size_obj;
+  let selected_spec_obj;
+  let grand_total;
+  try {
+    selected_size_obj = product.sizes.find((obj) => {
+      return obj.id === selected_size;
+    });
+  } catch (err) {
+    console.log("Error");
+  }
+  // if (selected_size_obj === undefined) {
+  //   selected_size_obj = "";
+  // }
+  try {
+    selected_spec_obj = product.specs.find((obj) => {
+      return obj.id === selected_special_op;
+    });
+  } catch (err) {
+    console.log("Error");
+  }
+  // if (selected_spec_obj === undefined) {
+  //   selected_spec_obj = "";
+  // }
+
+  // console.log("re-render");
+  // console.log(selected_size_obj);
+  // console.log(selected_spec_obj);
+
+  // if (selected_size_obj === "" || selected_spec_obj === "") {
+  //   grand_total = "";
+  // }
+  grand_total =
+    parseInt(selected_size_obj.price) + parseInt(selected_spec_obj.price);
+  // console.log(grand_total);
+
+  //============================
+  const [likeBtn, setLikeBtn] = useState(pink_heart);
+
+  const HandleHover = () => {
+    if (likeBtn === pink_heart) {
+      setLikeBtn(likeHover);
+    } else if (likeBtn === likeHover) {
+      setLikeBtn(pink_heart);
+    } else if (likeBtn === liked) {
+      setLikeBtn(liked);
+    }
+  };
+
+  const HandleLike = () => {
+    return likeBtn !== liked ? setLikeBtn(liked) : setLikeBtn(likeHover);
+  };
+
+  //============================
   const size_options_block = product.sizes.map((size) => (
     <SizeOption
       size={size.size}
@@ -41,65 +102,65 @@ export default function OrderSetup() {
   ));
 
   const special_option_block = product.specs.map((spec) => (
-    <SpecialOption price={spec.price} image={spec.img} id={spec.id} />
+    <SpecialOption
+      price={spec.price}
+      image={spec.img}
+      id={spec.id}
+      key={spec.id}
+    />
   ));
 
   return (
     <div className="order_setup">
-      <h1>{product.name}</h1>
-      <p>{product.description}</p>
-      <div className="available">
-        <img src={GreenCheckmark} />
-        <p>este în stock</p>
-      </div>
-      <div className="size_picker">
-        <p>Alegeti marimea</p>
-        <div className="size_options_div">
-          <SelectedSizeOption.Provider
-            value={[selected_size, set_selected_size]}
-          >
-            {size_options_block}
-            {/* <SizeOption size="mare" price="860" id="1" />
-            <SizeOption size="mediu" price="750" id="2" />
-            <SizeOption size="mic" price="650" id="3" /> */}
-          </SelectedSizeOption.Provider>
-        </div>
-      </div>
-      {/* ============ */}
-      <div className="special_options_picker">
-        <p>Faceți cadoul mai special</p>
-        <div className="special_options_div">
-          <SelectedSpecialOption.Provider
-            value={[selected_special_op, set_selected_special_op]}
-          >
-            {/* <SpecialOption price="123" id="1" />
-            <SpecialOption price="232" id="2" />
-            <SpecialOption price="456" id="3" />
-            <SpecialOption price="678" id="4" /> */}
-            {special_option_block}
-          </SelectedSpecialOption.Provider>
-        </div>
-      </div>
-      <div className="total_div">
-        <div className="total_amount">
-          <div>Suma totala</div>
-          <div>1200 MDL</div>
-        </div>
-        <div className="bought_item">
-          <p>Flori</p>
-          <p>860 mdl</p>
-        </div>
-        <div className="bought_item">
-          <p>Jucarie iepuras cu inima</p>
-          <p>340 mdl</p>
-        </div>
-      </div>
-      <div className="order_btn_div">
-        <button type="button" name="order">
-          Comanda
-        </button>
-        <img src={PinkHeart} className="product_page_like" />
-      </div>
+      <SelectedSizeOption.Provider value={[selected_size, set_selected_size]}>
+        <SelectedSpecialOption.Provider
+          value={[selected_special_op, set_selected_special_op]}
+        >
+          <h1>{product.name}</h1>
+          <p>{product.description}</p>
+          <div className="available">
+            <img src={GreenCheckmark} />
+            <p>este în stock</p>
+          </div>
+          <div className="size_picker">
+            <p>Alegeti marimea</p>
+            <div className="size_options_div">{size_options_block}</div>
+          </div>
+          {/* ============ */}
+          <div className="special_options_picker">
+            <p>Faceți cadoul mai special</p>
+            <div className="special_options_div">{special_option_block}</div>
+          </div>
+          <div className="total_div">
+            <div className="total_amount">
+              <div>Suma totala</div>
+              <div>{grand_total} MDL</div>
+            </div>
+            <div className="bought_item">
+              <p>
+                {product.name} ({selected_size_obj.size})
+              </p>
+              <p>{selected_size_obj.price} mdl</p>
+            </div>
+            <div className="bought_item">
+              <p>{selected_spec_obj.name}</p>
+              <p>{selected_spec_obj.price} mdl</p>
+            </div>
+          </div>
+          <div className="order_btn_div">
+            <button type="button" name="order">
+              Comanda
+            </button>
+            <img
+              src={likeBtn}
+              className="product_page_like"
+              onClick={HandleLike}
+              onMouseOver={HandleHover}
+              onMouseOut={HandleHover}
+            />
+          </div>
+        </SelectedSpecialOption.Provider>
+      </SelectedSizeOption.Provider>
     </div>
   );
 }
